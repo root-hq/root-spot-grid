72a1eb4b697d6879e72b3a3e59dc1088df02bc00
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-01-22 15:23:20 +0530

    Modify initializeMint script

--- scripts/market/initialize-market.ts
+++ scripts/market/initialize-market.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import * as rootSdk from "../../sdk";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 import axios from "axios";
+import { Client } from "@ellipsis-labs/phoenix-sdk";
 
 require("dotenv").config();
 
@@ -21,19 +22,26 @@ export const handler = async() => {
 
     const provider = new anchor.AnchorProvider(connection, wallet, {});
 
+    const PHOENIX_MARKET_KEY = new anchor.web3.PublicKey("3J9LfemPBLowAJgpG3YdYPB9n6pUk7HEjwgS6Y5ToSFg");
     const PROTOCOL_FEE_RECIPIENT = new anchor.web3.PublicKey("6HyM2raEk78s8PdiRKqSF36YtSZf3CjwmReTCtdaucuf");
+
+    const BASE_TOKEN_MINT = new anchor.web3.PublicKey("So11111111111111111111111111111111111111112");
+    const QUOTE_TOKEN_MINT = new anchor.web3.PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB");
 
     const withdrawalFeeInBpsHundredths = new anchor.BN(5000);
     const minOrderSizeInBaseLots = new anchor.BN(100);
     const minOrderSpacingInTicks = new anchor.BN(10);
+
+    const phoenixClient = await Client.create(connection);
+    await phoenixClient.addMarket(PHOENIX_MARKET_KEY.toString());
     
     const tx = await rootSdk.initializeMarket({
         provider,
-        phoenixMarket: rootSdk.PHOENIX_SOL_USDC_MAINNET,
+        phoenixMarket: PHOENIX_MARKET_KEY,
         owner: provider.wallet.publicKey,
         protocolFeeRecipient: PROTOCOL_FEE_RECIPIENT,
-        baseTokenMint: rootSdk.WRAPPED_SOL_MAINNET,
-        quoteTokenMint: rootSdk.USDC_MAINNET,
+        baseTokenMint: BASE_TOKEN_MINT,
+        quoteTokenMint: QUOTE_TOKEN_MINT,
         withdrawalFeeInBpsHundredths,
         minOrderSizeInBaseLots,
         minOrderSpacingInTicks
@@ -45,11 +53,11 @@ export const handler = async() => {
     });
 
     console.log("Spot grid market: ", tx.spotGridMarketAddress);
-    console.log("Phoenix market: ", rootSdk.PHOENIX_SOL_USDC_MAINNET);
+    console.log("Phoenix market: ", PHOENIX_MARKET_KEY);
     console.log("Spot grid market key: ", tx.spotGridMarketKey);
     console.log("Owner: ", provider.wallet.publicKey);
-    console.log("Base token mint: ", rootSdk.WRAPPED_SOL_MAINNET);
-    console.log("Quote token mint: ", rootSdk.USDC_MAINNET);
+    console.log("Base token mint: ", BASE_TOKEN_MINT);
+    console.log("Quote token mint: ", QUOTE_TOKEN_MINT);
     console.log("Withdrawal fee per fill bps hundredths: ", withdrawalFeeInBpsHundredths.toString());
     console.log("Min order size in base lots: ", minOrderSizeInBaseLots.toString());
     console.log("Min order spacing in ticks: ", minOrderSpacingInTicks.toString());
@@ -59,11 +67,11 @@ export const handler = async() => {
     const URL = `https://spot-grid-db-utils.vercel.app/api/`;
         const data = {
             "spot_grid_market_address": tx.spotGridMarketAddress,
-            "phoenix_market_address": rootSdk.PHOENIX_SOL_USDC_MAINNET,
+            "phoenix_market_address": PHOENIX_MARKET_KEY,
             "spot_grid_market_key": tx.spotGridMarketKey,
             "owner": provider.wallet.publicKey,
-            "base_token_mint": rootSdk.WRAPPED_SOL_MAINNET,
-            "quote_token_mint": rootSdk.USDC_MAINNET,
+            "base_token_mint": BASE_TOKEN_MINT,
+            "quote_token_mint": QUOTE_TOKEN_MINT,
             "withdrawal_fee_in_bps_hundredths": withdrawalFeeInBpsHundredths.toString(),
             "min_order_spacing_in_ticks": minOrderSizeInBaseLots.toString(),
             "min_order_size_in_base_lots": minOrderSpacingInTicks.toString(),
